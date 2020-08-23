d8ab0000aa0e5f55e5946c2fe04b2d8d31cecec6
root <sausofts@gmail.com>
2020-08-23 16:40:57 +0530

    add pages about contact , css

--- src/app/partials/footer/footer.component.ts
+++ src/app/partials/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Newsletterform } from './newsletterform';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
   constructor() { }
+  submitted = false;
+
+  onSubmit() { this.submitted = true;console.log('dddddddddddddddd '+this.email); }
+  
 
   ngOnInit() {
   }
-
+  
 }
